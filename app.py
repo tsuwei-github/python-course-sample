c1fb75ad2aad97f6384d0dec6e0814db507e04e3
tsuwei-github <r95521517@gmail.com>
2023-08-11 23:51:05 +0800

    6 commit

--- app.py
+++ app.py
@@ -19,6 +19,14 @@ app.config['MAX_CONTENT_LENGTH'] = 3 * 1024 * 1024
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
+def qr(filename):
+    img = cv2.imread(filename)
+    qrcode = cv2.QRCodeDetector()
+    data, bbox, rectified = qrcode.detectAndDecode(img)
+    if bbox is not None:
+        print(data)
+    return data
+
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -43,37 +51,24 @@ def upload_file():
     if file:
         file_type = filetype.guess_extension(file)
 
+
         if file_type in ALLOWED_EXTENSIONS:
             file.stream.seek(0)
             filename = secure_filename(file.filename)
-#######################
-
-            img = cv2.imread(filename)
-            qrcode = cv2.QRCodeDetector()
-            data, bbox, rectified = qrcode.detectAndDecode(img)
-
-            # 取得座標的函式
-            def boxSize(arr):
-                global data
-                box_roll = np.rollaxis(arr,1,0)   # 轉置矩陣，把 x 放在同一欄，y 放在同一欄
-                xmax = int(np.amax(box_roll[0]))  # 取出 x 最大值
-                xmin = int(np.amin(box_roll[0]))  # 取出 x 最小值
-                ymax = int(np.amax(box_roll[1]))  # 取出 y 最大值
-                ymin = int(np.amin(box_roll[1]))  # 取出 y 最小值
-                return (xmin,ymin,xmax,ymax)
-
-            # 如果 bbox 是 None 表示圖片中沒有 QRCode
-            if bbox is not None:
-                print(data)
-                # print(bbox)
-                # print(rectified)
-                box = boxSize(bbox[0])
-                cv2.rectangle(img,(box[0],box[1]),(box[2],box[3]),(0,0,255),5)  # 畫矩形
-#######################
-
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            
             # flash('影像上傳完畢！')
-            flash(data)
+            #######################
+            # img = cv2.imread(filename)
+            # qrcode = cv2.QRCodeDetector()
+            # data, bbox, rectified = qrcode.detectAndDecode(img)
+            # if bbox is not None:
+            # print(data)
+            #######################
+
+
+            flash(qr(filename))
+            flash(filename)
             return render_template('index.html', filename=filename)
         else:
             flash('僅允許上傳png, jpg, jpeg和gif影像檔')
