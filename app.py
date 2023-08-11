aca82ad403fc4102895944f678953b6a4f95328e
tsuwei-github <r95521517@gmail.com>
2023-08-11 22:32:37 +0800

    5 commit

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import pathlib
 import filetype
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
+import cv2
+import numpy as np
+
+
 
 # 取得目前檔案所在的資料夾
 SRC_PATH = pathlib.Path(__file__).parent.absolute()
@@ -42,8 +46,34 @@ def upload_file():
         if file_type in ALLOWED_EXTENSIONS:
             file.stream.seek(0)
             filename = secure_filename(file.filename)
+#######################
+
+            img = cv2.imread(filename)
+            qrcode = cv2.QRCodeDetector()
+            data, bbox, rectified = qrcode.detectAndDecode(img)
+
+            # 取得座標的函式
+            def boxSize(arr):
+                global data
+                box_roll = np.rollaxis(arr,1,0)   # 轉置矩陣，把 x 放在同一欄，y 放在同一欄
+                xmax = int(np.amax(box_roll[0]))  # 取出 x 最大值
+                xmin = int(np.amin(box_roll[0]))  # 取出 x 最小值
+                ymax = int(np.amax(box_roll[1]))  # 取出 y 最大值
+                ymin = int(np.amin(box_roll[1]))  # 取出 y 最小值
+                return (xmin,ymin,xmax,ymax)
+
+            # 如果 bbox 是 None 表示圖片中沒有 QRCode
+            if bbox is not None:
+                print(data)
+                # print(bbox)
+                # print(rectified)
+                box = boxSize(bbox[0])
+                cv2.rectangle(img,(box[0],box[1]),(box[2],box[3]),(0,0,255),5)  # 畫矩形
+#######################
+
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            flash('影像上傳完畢！')
+            # flash('影像上傳完畢！')
+            flash(data)
             return render_template('index.html', filename=filename)
         else:
             flash('僅允許上傳png, jpg, jpeg和gif影像檔')
@@ -57,3 +87,8 @@ def display_image(filename):
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
+
+
+
+
+
